38dc386c4b17f79a187b86e1145621757e2fb9d3
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2021-12-19 08:06:45 -0800

    commit tree 929628cbf1f7c6c3040a436aad1ba5ff2e0a026d

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, selectedCategory, onSearchChange, search }) {
+// function Filter({ onChange,  filterer }) {
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input type="text" name="search" placeholder="Search..." onChange={onSearchChange} value={search} />
+      {/* <input type="text" name="search" placeholder="Search..." onChange={onChange} value={filterer.search} /> */}
+      <select name="filter" onChange={onCategoryChange} value={selectedCategory}>
+      {/* <select name="filter" onChange={onChange} value={filterer.filter}> */}
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,41 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
-
+  const [searchBar, setSearchBar] = useState("");
+  // const [filterer, setFilterer] = useState( { filter: "All", search: "" } );
+  
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+  
+  function handleSearch(event) {
+    setSearchBar(event.target.value);
+  }
+
+  // function handleChange(event) {
+  //   const key = (event.target.name);
+  //   let value = (event.target.value);
+
+  //   setFilterer( {...filterer, [key]: value} );
+  // }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+    if (selectedCategory === "All" &&
+      item.name.toLowerCase().includes(searchBar.toLowerCase())
+    // if (filterer.filter === "All"  &&  item.name.toLowerCase().includes(filterer.search.toLowerCase())
+    ) return true; /* Personal Note: "One-liner" if statements don't appear to require braces? */
 
     return item.category === selectedCategory;
+    // return item.category === filterer.filter;
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter onCategoryChange={handleCategoryChange} selectedCategory={selectedCategory} onSearchChange={handleSearch} search={searchBar} />
+      {/* <Filter onChange={handleChange} filterer={filterer} /> */}
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
